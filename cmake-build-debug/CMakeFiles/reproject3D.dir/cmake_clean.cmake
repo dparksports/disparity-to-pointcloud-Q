2a0e93ca93b54cc110a33e97fa9418950bdff77b
Dan Park <dpark.sports@gmail.com>
2020-05-06 19:05:36 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/reproject3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reproject3D.dir/main.cpp.o"
+  "reproject3D"
+  "reproject3D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reproject3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
